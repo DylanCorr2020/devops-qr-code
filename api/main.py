b1730449c81be6960a214503a15bf3f91934fe22
Dylan Corr <dylancorr.g@gmail.com>
2025-01-25 14:53:00 +0000

    added docker files

--- api/main.py
+++ api/main.py
@@ -4,9 +4,8 @@ import qrcode
 import boto3
 import os
 from io import BytesIO
-
-# Loading Environment variable (AWS Access Key and Secret Key)
 from dotenv import load_dotenv
+
 load_dotenv()
 
 app = FastAPI()
@@ -26,40 +25,50 @@ app.add_middleware(
 # AWS S3 Configuration
 s3 = boto3.client(
     's3',
-    aws_access_key_id= os.getenv("AWS_ACCESS_KEY"),
-    aws_secret_access_key= os.getenv("AWS_SECRET_KEY"))
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_KEY")
+)
 
-bucket_name = 'YOUR_BUCKET_NAME' # Add your bucket name here
+bucket_name = 'devops-qr-code'
 
 @app.post("/generate-qr/")
 async def generate_qr(url: str):
-    # Generate QR Code
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(url)
-    qr.make(fit=True)
-
-    img = qr.make_image(fill_color="black", back_color="white")
-    
-    # Save QR Code to BytesIO object
-    img_byte_arr = BytesIO()
-    img.save(img_byte_arr, format='PNG')
-    img_byte_arr.seek(0)
-
-    # Generate file name for S3
-    file_name = f"qr_codes/{url.split('//')[-1]}.png"
-
+    print("Received URL:", url)  # Print the received URL
     try:
-        # Upload to S3
-        s3.put_object(Bucket=bucket_name, Key=file_name, Body=img_byte_arr, ContentType='image/png', ACL='public-read')
+        # Generate QR Code
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(url)
+        qr.make(fit=True)
+
+        print("QR code generated successfully.")  # After QR code is generated
+        img = qr.make_image(fill_color="black", back_color="white")
         
+        # Save QR Code to BytesIO object
+        img_byte_arr = BytesIO()
+        img.save(img_byte_arr, format='PNG')
+        img_byte_arr.seek(0)
+
+        # Generate file name for S3
+        file_name = f"qr_codes/{url.split('//')[-1]}.png"
+        print("Generated file name for S3:", file_name)  # Print generated file name
+
+        # Upload to S3
+        print("Uploading image to S3 bucket:", bucket_name)
+        s3.put_object(Bucket=bucket_name, Key=file_name, Body=img_byte_arr, ContentType='image/png', ACL='public-read')
+
+        print("Upload to S3 successful.")  # After successful upload
+
         # Generate the S3 URL
         s3_url = f"https://{bucket_name}.s3.amazonaws.com/{file_name}"
+        print("Generated S3 URL:", s3_url)  # Print the URL that will be returned
+
         return {"qr_code_url": s3_url}
+
     except Exception as e:
+        print("Error occurred:", str(e))  # Log the exception
         raise HTTPException(status_code=500, detail=str(e))
-    
